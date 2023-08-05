81fc874f40a5ba5dfca6308ade999483b8908151
Faridsyah <faridsyahalmuluk2@gmail.com>
2023-08-05 17:41:19 +0700

    menambahkan alert

--- js/script.js
+++ js/script.js
@@ -3,57 +3,55 @@ const listTodo = document.getElementsByClassName("list-todo");
 const listContainer = document.querySelectorAll(".list-container");
 const formTodo = document.querySelector(".container-form");
 
-// Fungsi saveData() bertujuan untuk menyimpan data todo ke dalam localStorage.
 function saveData() {
   localStorage.setItem("data", listTodo[0].innerHTML);
 
-  // Setelah data disimpan, memuat kembali data dari localStorage menggunakan fungsi loadData().
   loadData();
 }
 
-// Fungsi loadData() bertujuan untuk memuat data todo dari localStorage dan menampilkan di halaman.
 function loadData() {
-  // Mengambil elemen todo dari localStorage dan menambahkannya ke dalam listTodo[0].
   listTodo[0].innerHTML = localStorage.getItem("data");
 
-  // Mengambil semua elemen todo yang ada di listTodo[0].
+  // Mengambil semua elemet todo
   let todoNames = listTodo[0].querySelectorAll(".todo-name");
 
-  // Mengambil semua elemen delete (X) yang ada di listTodo[0].
+  // Mengambil semua elemen X
   let delTodo = listTodo[0].querySelectorAll(".x");
 
-  // Menambahkan Event Listener ke setiap elemen todo untuk toggle class "checked" saat diklik.
+  // Menambahkan Event Listener ke element todo-name
   todoNames.forEach(function (todoName, index) {
     todoName.addEventListener("click", function () {
       this.classList.toggle("checked");
-      saveData(); // Setelah setiap perubahan, data disimpan kembali ke localStorage.
+      saveData();
     });
 
-    // Menambahkan Event Listener ke setiap elemen delete (X) untuk menghapus todo saat diklik.
+    // Menambahkan Event Listener ke element logo delete
     let x = delTodo[index];
     x.addEventListener("click", function () {
       this.parentElement.remove();
-      saveData(); // Setelah setiap perubahan, data disimpan kembali ke localStorage.
-      checkListTodoKosong(); // Memeriksa apakah list todo kosong setelah menghapus.
+      saveData();
+      checkListTodoKosong();
     });
   });
 
-  // Memeriksa apakah list todo kosong atau tidak untuk menampilkan/menyembunyikan elemen lain.
+  // Cek jika tidak ada element di dalam list todo
   if (listTodo[0].children.length > 0) {
-    // Jika list todo tidak kosong, maka tampilkan elemen container-form dan hapus class "non-active" dari list-container.
+    // Menghapus class "non-active" dari class list-container
     listContainer[0].classList.remove("non-active");
-    formTodo.classList.add("v2"); // Tambahkan margin-top pada .container-form.
+    // Menambah margin-top pada .container-form
+    formTodo.classList.add("v2");
   } else {
-    // Jika list todo kosong, maka sembunyikan elemen container-form dan tambahkan class "non-active" pada list-container.
+    // Menambahkan class "non-active" ke class list-container
     listContainer[0].classList.add("non-active");
-    formTodo.classList.remove("v2"); // Hapus margin-top pada .container-form.
+    // Menghapus margin-top pada .container-form
+    formTodo.classList.remove("v2");
   }
 }
 
-// Fungsi addTask() bertujuan untuk menambahkan tugas baru ke dalam list todo.
+loadData();
+
 function addTask() {
   if (inputBox.value === "") {
-    // Jika inputBox kosong, tampilkan pesan alert "You have to write something!".
     let alertText = document.querySelector(".container-form form p");
     if (!alertText) {
       const alert = document.createElement("p");
@@ -64,73 +62,73 @@ function addTask() {
       alertCont();
     }
   } else {
-    // Jika inputBox tidak kosong, hilangkan pesan alert (jika ada).
     let alertText = document.querySelector(".container-form form p");
+
     if (alertText) {
-      alertText.classList.add("not-active");
-    } else {
-      alertText.classList.remove("not-active");
+      alertText.remove();
     }
 
-    // Membuat elemen <ul> baru untuk menampung todo baru.
+    // Membuat elemen <ul> baru
     let ul = document.createElement("ul");
 
-    // Membuat elemen <div> dengan class "todo-name".
+    // Membuat elemen <div> dengan class "todo-name"
     let todoNameDiv = document.createElement("div");
     todoNameDiv.className = "todo-name";
 
-    // Membuat elemen <p> untuk menampilkan teks tugas.
+    // Membuat elemen <p> untuk teks tugas
     let taskText = document.createElement("p");
     taskText.textContent = inputBox.value;
     todoNameDiv.appendChild(taskText);
 
-    // Menambahkan event listener untuk toggle class "checked" saat elemen todo diklik.
+    // Menambahkan event listener untuk mengubah class saat diklik
     todoNameDiv.addEventListener("click", function () {
       this.classList.toggle("checked");
-      saveData(); // Setelah setiap perubahan, data disimpan kembali ke localStorage.
+      saveData();
     });
 
     ul.appendChild(todoNameDiv);
 
-    // Membuat elemen <div> dengan class "x" untuk tombol delete.
+    // Membuat elemen <div> dengan class "x"
     let xDiv = document.createElement("div");
     xDiv.className = "x";
     ul.appendChild(xDiv);
 
-    // Menambahkan elemen <ul> ke dalam elemen dengan class "list-todo".
+    // Menambahkan elemen <ul> ke dalam elemen dengan class "list-todo"
     listTodo[0].appendChild(ul);
-    saveData(); // Setelah menambahkan todo baru, data disimpan kembali ke localStorage.
+    saveData();
 
-    // Menambahkan event listener pada tombol delete (X) untuk menghapus todo saat diklik.
+    // Menambahkan event listener untuk menghapus list todo saat diklik x
     xDiv.addEventListener("click", function () {
       this.parentElement.remove();
-      saveData(); // Setelah setiap perubahan, data disimpan kembali ke localStorage.
-      checkListTodoKosong(); // Memeriksa apakah list todo kosong setelah menghapus.
+      saveData();
+      // Menjalankan Fungsi checkListTodoKosong
+      checkListTodoKosong();
     });
 
-    // Menghapus teks dari inputBox setelah tugas ditambahkan.
+    // Menghapus teks dari inputBox setelah tugas ditambahkan
     inputBox.value = "";
-    saveData(); // Setelah setiap perubahan, data disimpan kembali ke localStorage.
+    saveData();
   }
 
-  // Memeriksa apakah list todo kosong atau tidak untuk menampilkan/menyembunyikan elemen lain.
+  // Cek jika tidak ada element di dalam list todo
   if (listTodo[0].children.length > 0) {
-    // Jika list todo tidak kosong, maka tampilkan elemen container-form dan hapus class "non-active" dari list-container.
+    // Menghapus class "non-active" dari class list-container
     listContainer[0].classList.remove("non-active");
   } else {
-    // Jika list todo kosong, maka sembunyikan elemen container-form dan tambahkan class "non-active" pada list-container.
+    // Menambahkan class "non-active" ke class list-container
     listContainer[0].classList.add("non-active");
   }
 }
 
-// Fungsi checkListTodoKosong() bertujuan untuk memeriksa apakah list todo kosong atau tidak.
 function checkListTodoKosong() {
-  // Jika list todo kosong, maka sembunyikan elemen container-form dan hapus margin-top pada .container-form.
+  // Cek jika tidak ada elemen di dalam list-todo
   if (listTodo[0].children.length === 0) {
+    // Menambahkan class "non-active" ke class list-container
     listContainer[0].classList.add("non-active");
+    // Menghapus margin-top pada .container-form
     formTodo.classList.remove("v2");
   } else {
-    // Jika list todo tidak kosong, maka tampilkan elemen container-form dan tambahkan margin-top pada .container-form.
+    // Menambah margin-top pada .container-form
     formTodo.classList.add("v2");
   }
 }
